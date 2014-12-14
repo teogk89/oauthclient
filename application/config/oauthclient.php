0cc71d44f38a1be24ea59e622b4d02eb8fbffa41
teogk89 <teogk89@gmail.com>
2014-12-14 13:53:16 +0700

    edit the name

--- application/config/oauthclient.php
+++ application/config/oauthclient.php
@@ -2,11 +2,11 @@
 
 
 
-$config['runkeeper']['clientId'] = "6172aa239d0e406b92a32394bfd2a1ac";
-$config['runkeeper']['clientSecret'] = "e97642c9bbfc455887e8c0bdd091b0aa";
-$config['runkeeper']['redirectUri'] = "http://duykhanh.net/oauthClient/index.php/activities";
+$config['Runkeeper']['clientId'] = "6172aa239d0e406b92a32394bfd2a1ac";
+$config['Runkeeper']['clientSecret'] = "e97642c9bbfc455887e8c0bdd091b0aa";
+$config['Runkeeper']['redirectUri'] = "http://duykhanh.net/oauthClient/index.php/activities";
 
 
-$config['mapmyapi']['clientId'] = "zjpskrkak6t3xc3682j5tarkknjef74m";
-$config['mapmyapi']['clientSecret'] = "5Q6nYDscFfcyuuRU28e8FbapKBGzNfxDsQEbTw22fjv";
-$config['mapmyapi']['redirectUri'] = "http://localhost2:82/oauthClient/index.php/mapmyapi";
+$config['Mapmyapi']['clientId'] = "zjpskrkak6t3xc3682j5tarkknjef74m";
+$config['Mapmyapi']['clientSecret'] = "5Q6nYDscFfcyuuRU28e8FbapKBGzNfxDsQEbTw22fjv";
+$config['Mapmyapi']['redirectUri'] = "http://localhost2:82/oauthClient/index.php/mapmyapi";
